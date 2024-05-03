04db353a7eb8d0f9060029ef826e25fa8225b408
Nikolai2038 <nikolai2038@yandex.ru>
2024-05-03 17:04:53 +0300

    Tk variant

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import cv2
 import numpy as np
+from tkinter import Tk, Button, Label, messagebox
+from PIL import Image, ImageTk
 
 
 def add_image(frame, overlay, alpha):
@@ -21,109 +23,112 @@ def template_matching(frame, template):
 
     return frame
 
+overlay_image = cv2.imread('./input/iknk.jpg')
+template_image = cv2.imread('./input/poly.png')
 
-# 1
-cap = cv2.VideoCapture(0)
-# To fix "global cap_v4l.cpp:1134 tryIoctl VIDEOIO(V4L2:/dev/video0): select() timeout."
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+class VideoCapture:
+    def __init__(self):
+        self.capture = cv2.VideoCapture(0)
 
-# 3
-# cap=cv2.VideoCapture(0, cv2.CAP_V4L)
-cap.set(3,640)#width
-cap.set(4,480)#height
-cap.set(10,100)#brightness
+        # To fix "global cap_v4l.cpp:1134 tryIoctl VIDEOIO(V4L2:/dev/video0): select() timeout."
+        self.capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('./output/result.avi', fourcc, 30.0, (640, 480))
+        # width
+        self.capture.set(3, 640)
+        # height
+        self.capture.set(4, 480)
+        # brightness
+        self.capture.set(10, 100)
+
+        self.video_writer = None
+        self.running = False
+
+    def start_capture(self):
+        if not self.running:
+            self.running = True
+            self.video_writer = cv2.VideoWriter('./output/result.avi', cv2.VideoWriter_fourcc(*'XVID'), 30.0, (640, 480))
+
+    def stop_capture(self):
+        if self.running:
+            self.running = False
+            self.video_writer.release()
+
+    def update_preview(self):
+        ret, frame = self.capture.read()
+        if ret:
+            #alpha = cv2.getTrackbarPos('Alpha', 'frame') / 10
+            alpha = 0
+            frame = add_image(frame, overlay_image, alpha)
+            frame = template_matching(frame, template_image)
+            #cv2.imshow('frame', frame)
+
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.label.imgtk = imgtk
+            self.label.config(image=imgtk)
+
+            if self.running:
+                self.video_writer.write(frame)
+        root.after(30, self.update_preview)
+
+    def __del__(self):
+        self.capture.release()
+        self.video_writer.release()
+        cv2.destroyAllWindows()
 
 cv2.namedWindow('frame')
 cv2.createTrackbar('Alpha', 'frame', 0, 10, lambda x: None)
 
-overlay_image = cv2.imread('./input/iknk.jpg')
-template_image = cv2.imread('./input/poly.png')
+root = Tk()
+vcapture = VideoCapture()
+vcapture.label = Label(root)
+vcapture.label.pack()
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret:
-        alpha = cv2.getTrackbarPos('Alpha', 'frame') / 10
-        frame = add_image(frame, overlay_image, alpha)
-        frame = template_matching(frame, template_image)
-        out.write(frame)
-        cv2.imshow('frame', frame)
-    else:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+start_button = Button(root, text="Start", command=vcapture.start_capture)
+start_button.pack(side="left")
 
-    key_pressed = cv2.waitKey(1)
-    if key_pressed == ord('q'):
-        break
-    # ESC pressed
-    elif key_pressed % 256 == 27:
-        print("Escape hit, closing...")
-        break
-    # SPACE pressed
-    elif key_pressed % 256 == 32:
-        img_name = "./output/opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+stop_button = Button(root, text="Stop", command=vcapture.stop_capture)
+stop_button.pack(side="right")
+
+exit_button = Button(root, text="Exit", command=root.destroy)
+exit_button.pack(side="bottom")
+
+# key_pressed = cv2.waitKey(1)
+# # Q, ESC or SPACE
+# if (key_pressed == ord('q')) or (key_pressed % 256 == 27) or (key_pressed % 256 == 32):
+#     exit(0)
+
+vcapture.update_preview()
+root.mainloop()
 
 # Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
 
 #
-# import numpy as np
-# import cv2
 #
-# video_capture = cv2.VideoCapture(0)
+# # 1
+# cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 #
-# while(True):
-#     # Capture frame-by-frame
-#     ret, frame = video_capture.read()
+# # 3
+# # cap=cv2.VideoCapture(0, cv2.CAP_V4L)
 #
-#     # Our operations on the frame comes here
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('./output/result.avi', fourcc, 30.0, (640, 480))
 #
-#     # Display the resulting frame
-#     cv2.imshow('frame',gray)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#
+# cv2.labl
+#
+#
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     if ret:
+#         alpha = cv2.getTrackbarPos('Alpha', 'frame') / 10
+#         frame = add_image(frame, overlay_image, alpha)
+#         frame = template_matching(frame, template_image)
+#         out.write(frame)
+#         cv2.imshow('frame', frame)
+#     else:
+#         print("Can't receive frame (stream end?). Exiting ...")
 #         break
 #
-# # When everything's done, release the capture
-# video_capture.release()
-# cv2.destroyAllWindows()
-#
-#
-# import cv2
-#
-# cam = cv2.VideoCapture(0)
-# cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-#
-# cv2.namedWindow("test")
-#
-# img_counter = 0
-#
-# while True:
-#     ret, frame = cam.read()
-#     if not ret:
-#         print("failed to grab frame")
-#         break
-#     cv2.imshow("test", frame)
-#
-#     k = cv2.waitKey(1)
-#     # ESC pressed
-#     if k % 256 == 27:
-#         print("Escape hit, closing...")
-#         break
-#     # SPACE pressed
-#     elif k % 256 == 32:
-#         img_name = "./output/opencv_frame_{}.png".format(img_counter)
-#         cv2.imwrite(img_name, frame)
-#         print("{} written!".format(img_name))
-#         img_counter += 1
-#
-# cam.release()
-#
-# cv2.destroyAllWindows()
